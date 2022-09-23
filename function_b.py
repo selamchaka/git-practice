952cfd7d4d981698de246dd8894577894771231a
Selam Chaka <selblessed@gmail.com>
2022-09-23 17:05:46 -0400

    changed the fuction a

--- function_b.py
+++ function_b.py
@@ -8,7 +8,23 @@ def silly_sum():
         the sum reaches or exceeds 1000
     """
     pass
+def most_common_value(number_list):
+    """ returns the most common element of the list
+    """
+    frequency_index = {}
+    max_frequency = -1
+    most_common_value = None
+    for num in number_list:
+        if frequency_index.get(num):
+            frequency_index[num] += 1
+        else:
+            frequency_index[num] = 1
 
+        if max_frequency < frequency_index[num]:
+            max_frequency = frequency_index[num]
+            most_common_value = num
+
+    return most_common_value
 
 if __name__ == "__main__":
     print(f"Answer = {silly_sum()}")
